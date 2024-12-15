12df5029f4ecbe995a63878e2279f96256d0a9ba
BugHunter-X <askzenai@gmail.com>
2024-12-15 13:48:04 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,8 @@
+function myFunc() {
+  // Some code here
+  if (condition) {
+    return; // Or return a specific value
+  }
+  // More code here
+  return true; // Explicit return to avoid implicit NULL
+} 
